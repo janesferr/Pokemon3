c98c0bafa75bf7aa67be3d7da2b4ee404aa0b2a4
Jane Sferrazza <sferrazzajane@gmail.com>
2018-12-30 20:24:03 -0500

    fixup imgs id

--- main.js
+++ main.js
@@ -69,38 +69,33 @@ axios.get("https://fizal.me/pokeapi/api/v2/id/500.json")
     console.log(response.data);
     drawCard('card-emboar', response.data);
   });
-function image_enlarge(img01)
-{
-// Get the modal
-var modal = document.getElementById('myModal');
+  
+function image_enlarge(img_x) {
+  // Get the modal
+  var modal = document.getElementById('myModal');
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById('myImg');
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function () {
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+  // Get the image and insert it inside the modal - use its "alt" text as a caption
+  var img = document.getElementById(img_x);
+  var modalImg = document.getElementById("img_modal");
+  var captionText = document.getElementById("caption");
+  img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
 
-  // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+      modal.style.display = "none";
+    }
+  }
 }
-}
-}
 
-
-
-
-
-
-image_enlarge(img01);
-image_enlarge(img02);
-image_enlarge(img03);
+image_enlarge("img01");
+image_enlarge("img02");
+image_enlarge("img03");
 
 function trainer() {
   var variable = document.getElementById('input_id').value;
